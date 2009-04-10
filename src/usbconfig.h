5680966b239b30a5467cb208223b4def35ec9266
Josh Kropf <josh@slashdev.ca>
2009-04-10 15:53:20 -0400

    Switched USB PID/VID to shared IDs from Objective Development

--- src/usbconfig.h
+++ src/usbconfig.h
@@ -129,14 +129,14 @@ rename it to "usbconfig.h". Then edit it accordingly.
 
 /* -------------------------- Device Description --------------------------- */
 
-#define  USB_CFG_VENDOR_ID       0x81, 0x17
+#define  USB_CFG_VENDOR_ID       0xc0, 0x16
 /* USB vendor ID for the device, low byte first. If you have registered your
  * own Vendor ID, define it here. Otherwise you use obdev's free shared
  * VID/PID pair. Be sure to read USBID-License.txt for rules!
  * This template uses obdev's shared VID/PID pair for HIDs: 0x16c0/0x5df.
  * Use this VID/PID pair ONLY if you understand the implications!
  */
-#define  USB_CFG_DEVICE_ID       0x96, 0x0a
+#define  USB_CFG_DEVICE_ID       0xdf, 0x05
 /* This is the ID of the product, low byte first. It is interpreted in the
  * scope of the vendor ID. If you have registered your own VID with usb.org
  * or if you have licensed a PID from somebody else, define it here. Otherwise
@@ -148,7 +148,7 @@ rename it to "usbconfig.h". Then edit it accordingly.
 #define USB_CFG_DEVICE_VERSION  0x00, 0x01
 /* Version number of the device: Minor number first, then major number.
  */
-#define USB_CFG_VENDOR_NAME     'r', 'a', 'p', 'h', 'n', 'e', 't', '.', 'n', 'e', 't'
+#define USB_CFG_VENDOR_NAME     's', 'l', 'a', 's', 'h', 'd', 'e', 'v', '.', 'c', 'a'
 #define USB_CFG_VENDOR_NAME_LEN 11
 /* These two values define the vendor name returned by the USB device. The name
  * must be given as a list of characters under single quotes. The characters
@@ -158,7 +158,7 @@ rename it to "usbconfig.h". Then edit it accordingly.
  * obdev's free shared VID/PID pair. See the file USBID-License.txt for
  * details.
  */
-#define USB_CFG_DEVICE_NAME     'U', 'S', 'B', '_', 'G', 'a', 'm', 'e', '1', '2'
+#define USB_CFG_DEVICE_NAME     'P', 'S', '3', '_', 'A', 'r', 'c', 'a', 'd', 'e'
 #define USB_CFG_DEVICE_NAME_LEN 10
 /* Same as above for the device name. If you don't want a device name, undefine
  * the macros. See the file USBID-License.txt before you assign a name if you
